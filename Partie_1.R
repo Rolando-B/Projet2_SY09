8b90393c07a7dee4c793d01d74e8e60edc151716
Bastien ROLANDO <bastien.rolando@gmail.com>
2018-06-18 16:10:14 +0200

    fit and predict on each function/dataset

--- Partie_1.R
+++ Partie_1.R
@@ -1,14 +1,15 @@
 library(e1071)
 library(MASS)
-library(fossil)
 source("functions/anadisc.R")
 source("functions/mvdnorm.R")
 source("functions/prob.ad.R")
+source("functions/tree.rpart.R")
 source("functions/prepare.R")
 source("functions/separ2.R")
 source("functions/distXY.R")
 source("functions/protocole.R")
 source("functions/protocole2.R")
+source("functions/logistic.R")
 source("functions/get.best.variables.R")
 
 
@@ -19,6 +20,10 @@ XappCancer <- cancer[,2:31]
 zappCancer <- cancer$Z
 resCancer <- protocole2(XappCancer,zappCancer, quadraticVariablesCancer)
 
+resCancer$mean
+resCancer$sd
+plot(prcomp(XappCancer)$x[,1], prcomp(XappCancer)$x[,2], col=c("red", "blue")[zappCancer])
+
 #2. Ionosphere
 iono <- read.csv("data/ionosphere.csv", header=T)
 di = names(which(diag(cov(iono)) == 0)) #We remove columns with cov == 0
@@ -28,25 +33,43 @@ XappIono <- iono[,3:34]
 zappIono <- iono$Z
 quadraticVariablesIono <- get.best.variables(cbind(XappIono,Z = zappIono),8)
 resIono <- protocole2(XappIono,as.integer(zappIono), quadraticVariablesIono)
+resIono$mean
+resIono$sd
+plot(prcomp(XappIono)$x[,1], prcomp(XappIono)$x[,2], col=c("red", "blue")[zappIono])
 
 
 #3. Sonar
 sonar <- read.csv("data/sonar.csv", header=T)
 XappSonar <- sonar[,3:61]
-quadraticVariablesSonar <- get.best.variables(XappSonar)
 zappSonar <- sonar$Z
+quadraticVariablesSonar <- get.best.variables(cbind(XappSonar,Z = zappSonar),10)
 resSonar <- protocole2(XappSonar,zappSonar,quadraticVariablesSonar)
+resSonar$mean
+resSonar$sd
+plot(prcomp(XappSonar)$x[,1], prcomp(XappSonar)$x[,2], col=c("red", "blue")[zappSonar])
+
 
 #4. Spam
 spam <- read.csv("data/spambase.csv", header=T)
 XappSpam <- spam[,3:58]
-quadraticVariablesSpam <- get.best.variables(XappSpam)
 zappSpam <- spam$Z
+quadraticVariablesSpam <- get.best.variables(cbind(XappSpam,Z = zappSpam),8)
 resSpam <- protocole2(XappSpam,zappSpam,quadraticVariablesSpam)
+
+plot(prcomp(XappSpam[3:54])$x[,1], prcomp(XappSpam[3:54])$x[,2], col=c("red", "blue")[zappSpam])
+
+resSpam$mean
+resSpam$sd
 
 #5. Spam 2
 spam2 <- read.csv("data/spambase2.csv", header=T)
 XappSpam2 <- spam2[,3:58]
-quadraticVariablesSpam2 <- get.best.variables(XappSpam2)
 zappSpam2 <- spam2$Z
+quadraticVariablesSpam2 <- get.best.variables(cbind(XappSpam2,Z = zappSpam2),8)
 resSpam2 <- protocole2(XappSpam2,zappSpam2,quadraticVariablesSpam2)
+
+plot(prcomp(XappSpam2)$x[,1], prcomp(XappSpam2)$x[,2], col=c("red", "blue")[zappSpam])
+
+
+resSpam2$mean
+resSpam2$sd
--- functions/protocole2.R
+++ functions/protocole2.R
@@ -1,31 +1,49 @@
 protocole2 = function (Xapp, zapp, quadraticVariables= NULL,N = 20){
   
-  taux = matrix(0, N, 3)
-  colnames(taux) = c("adq", "adl", "nba")
+  taux = matrix(0, N, 8)
+  colnames(taux) = c("adq","adqVars", "adl", "adlVars", "nba","log", "logVars","tree")
   taux = data.frame(taux)
   
   for (i in c(0:N)){
     
     splitData = separ2(Xapp,zapp)
     datafApp = cbind(splitData$Xapp, Z=as.factor(splitData$zapp))
+    datafAppVars = cbind(splitData$Xapp[quadraticVariables], Z=as.factor(splitData$zapp))
     datafVal = cbind(splitData$Xval, Z=as.factor(splitData$zval))
-    if (is.null(quadraticVariables)){
+    datafValVars = cbind(splitData$Xval[quadraticVariables], Z=as.factor(splitData$zval))
+    
+    try({
       paramsAdq = qda(splitData$Xapp, factor(splitData$zapp))
       resAdq = predict(paramsAdq,splitData$Xval)
-    }
-    else{
-      paramsAdq = qda(splitData$Xapp[quadraticVariables], factor(splitData$zapp))
-      resAdq = predict(paramsAdq,splitData$Xval[quadraticVariables])
-    }
+      taux$adq[i] = 1 - rand.index(as.integer(resAdq$class),splitData$zval)
+    })
+    paramsAdqVars = qda(splitData$Xapp[quadraticVariables], factor(splitData$zapp))
     paramsAdl = lda(splitData$Xapp, splitData$zapp)
+    paramsAdlVars = lda(splitData$Xapp[quadraticVariables], splitData$zapp)
     paramsNba = naiveBayes(Z ~ .,data = datafApp)
+    paramsLog = glm(Z ~.,family=binomial(link='logit'),data = datafApp)
+    paramsLogVars = glm(Z ~.,family=binomial(link='logit'),data = datafAppVars)
     
+    resTree = tree.app(splitData$Xapp,splitData$zapp,splitData$Xval)
+    
+    
+    resAdqVars = predict(paramsAdqVars,splitData$Xval[quadraticVariables])
     resAdl = predict(paramsAdl,splitData$Xval)
+    resAdlVars = predict(paramsAdlVars,splitData$Xval[quadraticVariables])
     resNba = predict(paramsNba,datafVal, type="class")
+    resLog = predict(paramsLog,datafVal, type="response")
+    resLogVars = predict(paramsLogVars,datafValVars, type="response")
+    resLog <- ifelse(resLog > 0.5,1,2)
+    resLog <- ifelse(resLogVars > 0.5,1,2)
     
-    taux$adq[i] = 1 - rand.index(as.integer(resAdq$class),splitData$zval)
+
+    taux$adqVars[i] = 1 - rand.index(as.integer(resAdqVars$class),splitData$zval)
     taux$adl[i] = 1 - rand.index(as.integer(resAdl$class),splitData$zval)
+    taux$adlVars[i] = 1 - rand.index(as.integer(resAdlVars$class),splitData$zval)
     taux$nba[i] = 1 - rand.index(as.integer(resNba),splitData$zval)
+    taux$log[i] = 1 - rand.index(as.integer(resLog),splitData$zval)
+    taux$logVars[i] = 1 - rand.index(as.integer(resLogVars),splitData$zval)
+    taux$tree[i] = 1 - rand.index(as.integer(resTree$pred),splitData$zval)
     
   }
   
